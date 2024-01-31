d26f0a3650b943a8ed8dbf02464ac4108210d249
adugnata <a.tesfamichael.a@gmail.com>
2024-01-31 13:41:48 -0600

    add menu

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import architectureImage from '../image/architecture.png';
 function Blog({ blogData: blog }) {
     return (
         <div className="blog-container">
             <h2 className="blog-title">{blog.title}</h2>
+            {/* Add the architecture image here */}
+            <img src={architectureImage} alt="Architecture Diagram" className="blog-architecture-image"/>
             {blog.sections.map((section, index) => (
                 <div className="blog-section" key={index}>
                     {section.subtitle && <h3 className="blog-subtitle">{section.subtitle}</h3>}
@@ -15,3 +17,4 @@ function Blog({ blogData: blog }) {
 }
 
 export default Blog;
+
